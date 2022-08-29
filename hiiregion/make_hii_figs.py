6282be8e441c0ad9f921dca9ec6b2d1a5ea2e808
Marc Pound <22331890+mpound@users.noreply.github.com>
2022-08-29 15:49:46 -0400

    update text with reference links and last paragraph caveat. change y axis limit of figures

--- hiiregion/make_hii_figs.py
+++ hiiregion/make_hii_figs.py
@@ -9,34 +9,36 @@ from pdrtpy.modelset import ModelSet
 from pdrtpy.plot.modelplot import ModelPlot
 import astropy.units as u
 
+nax1clip = [1E3,7E3]
+nax2clip = [10,1E6]
 ms = ModelSet(name="wk2020",z=1)
 mp = ModelPlot(ms)
-mp.phasespace(["ARIII_21.83/ARIII_8.99","ARV_13.07/ARV_7.91"],nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+mp.phasespace(["ARIII_21.83/ARIII_8.99","ARV_13.07/ARV_7.91"],nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 
 mp.savefig("ARIII_21.83_8.99-ARV_13.07_7.91.png",dpi=600)
 mp.savefig("ARIII_21.83_8.99-ARV_13.07_7.91.pdf")
 
 mp.phasespace(['FEII_1.60/FEII_1.64','FEII_1.64/FEII_5.34'],
-              nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+              nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 mp.savefig("FeII_1.60_1.64-1.64_5.34.png",dpi=600)
 mp.savefig("FeII_1.60_1.64-1.64_5.34.pdf")
 
 mp.phasespace(['FEII_5.67/FEII_5.34','FEII_1.64/FEII_5.34'],
-              nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+              nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 mp.savefig("FeII_5.67_5.34-1.64_5.34.png",dpi=600)
 mp.savefig("FeII_5.67_5.34-1.64_5.34.pdf")
 
 mp.phasespace(['FEII_17.94/FEII_5.34','FEII_1.64/FEII_5.34'],
-              nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+              nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 mp.savefig("FeII_17.94_5.34-1.64_5.34.png",dpi=600)
 mp.savefig("FeII_17.94_5.34-1.64_5.34.pdf")
 
 mp.phasespace(['FEII_1.60/FEII_1.64','FEII_22.90/FEII_5.34'],
-              nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+              nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 mp.savefig("FeII_1.60_1.64-22.90_5.34.png",dpi=600)
 mp.savefig("FeII_1.60_1.64-22.90_5.34.pdf")
 
 mp.phasespace(['FEII_1.60/FEII_1.64','FEII_26/FEII_5.34'],reciprocal=[False,False],
-              nax2_clip=[10,1E6]*u.Unit("cm-3"),nax1_clip=[2E3,1E4]*u.Unit("K"))
+              nax2_clip=nax2clip*u.Unit("cm-3"),nax1_clip=nax1clip*u.Unit("K"))
 mp.savefig("FeII_1.60_1.64-25.99_5.34.png",dpi=600)  
 mp.savefig("FeII_1.60_1.64-25.99_5.34.pdf")
